81ff14fe115e877088ebdd2235a41f0b5b4d6c2e
sbabukov <s.babukov@gmail.com>
2023-05-14 09:09:27 +0300

    Initial commit of Custom Theme

---
+++ wp-content/themes/softuni-shop/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Online Shop</title>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+
+	<link rel="stylesheet" href="./css/master.css">
+	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+</head>
+<body>
+	<div class="site-wrapper">
+		<header class="site-header">
+			<h1 class="site-title"><a href="#">Online Shop</a></h1>
+		</header>
+
+		<ul class="products-listing">
+			<li class="product-card">
+				<div class="product-primary">
+					<h2 class="product-title"><a href="#">HAIER HW80-B14939-S 8.0 kg, 1400 rot/min</a></h2>
+					<div class="product-meta">
+						<a class="meta-shockcode" href="#">Code: 650204111</a>
+						<span class="meta-price">$ 179.99</span>
+					</div>
+					<div class="product-details product-details-table">
+						<span>Type</span><span>Washing machine</span>	
+						<span>Brand</span><span>HAIER</span>
+						<span>Model</span><span>HW80-B14939-S</span>
+					</div>
+				</div>
+				<div class="product-logo">
+					<div class="product-logo-box">
+						<img src="images/washing-machine.jpg" alt="">
+					</div>
+				</div>
+			</li>
+
+			<li class="product-card">
+				<div class="product-primary">
+					<h2 class="product-title"><a href="#">HAIER HW80-B14939-S2 8.0 kg, 1400 rot/min</a></h2>
+					<div class="product-meta">
+						<a class="meta-shockcode" href="#">Code: 650204112</a>
+						<span class="meta-price">$179.99</span>
+					</div>
+					<div class="product-details product-details-table">
+						<span>Type</span><span>Washing machine</span>	
+						<span>Brand</span><span>HAIER</span>
+						<span>Model</span><span>HW80-B14939-S2</span>
+					</div>
+				</div>
+				<div class="product-logo">
+					<div class="product-logo-box">
+						<img src="images/washing-machine.jpg" alt="">
+					</div>
+				</div>
+			</li>
+		</ul>
+		
+		<footer class="site-footer">
+			<p>Copyright 2020 | Developer links: 
+				<a href="/edits.html">Edits</a>,
+				<a href="/index.html">Home</a>,
+				<a href="/single.html">Single</a>
+			</p>
+		</footer>
+	</div>
+
+</body>
+</html>
